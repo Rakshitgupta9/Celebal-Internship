3fd6e8e69e2c7da62d9f297f4551f1c817e19392
Rakshit Gupta <95240061+Rakshitgupta9@users.noreply.github.com>
2024-06-08 15:02:47 +0530

    Add files via upload

---
+++ Project 1 (Replace NULLs)/assignment 1.sql
@@ -0,0 +1,59 @@
+CREATE TABLE HZL_Table (
+    Date DATE,
+    BU VARCHAR(10),
+    Value INT
+);
+
+INSERT INTO HZL_Table (Date, BU, Value) VALUES
+('2024-01-01', 'hzl', 3456),
+('2024-02-01', 'hzl', NULL),
+('2024-03-01', 'hzl', NULL),
+('2024-04-01', 'hzl', NULL),
+('2024-01-01', 'SC', 32456),
+('2024-02-01', 'SC', NULL),
+('2024-03-01', 'SC', NULL),
+('2024-04-01', 'SC', NULL),
+('2024-05-01', 'SC', 345),
+('2024-06-01', 'SC', NULL);
+
+SELECT * from HZL_Table;
+
+WITH CTE AS (
+    SELECT
+        Date,
+        BU,
+        Value,
+        ROW_NUMBER() OVER (PARTITION BY BU ORDER BY Date) AS RN
+    FROM
+        HZL_Table
+),
+RECURSIVE_CTE AS (
+    SELECT
+        Date,
+        BU,
+        Value,
+        RN
+    FROM
+        CTE
+    WHERE
+        Value IS NOT NULL
+    UNION ALL
+    SELECT
+        CTE.Date,
+        CTE.BU,
+        COALESCE(CTE.Value, RECURSIVE_CTE.Value) AS Value,
+        CTE.RN
+    FROM
+        CTE
+    JOIN
+        RECURSIVE_CTE ON CTE.BU = RECURSIVE_CTE.BU AND CTE.RN = RECURSIVE_CTE.RN + 1
+)
+SELECT
+    Date,
+    BU,
+    Value
+FROM
+    RECURSIVE_CTE
+ORDER BY
+    BU,
+    Date;
